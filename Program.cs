0cc6650c16a751adff010b6ad9c64939efe232c8
Jessica <jpanndora@gmail.com>
2025-09-20 03:17:30 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string fraseDigitada, fraseComDislalia;
+
+Console.Write("Digite uma flase: ");
+fraseDigitada = Console.ReadLine()!;
+fraseComDislalia = fraseDigitada.Replace("r", "l").Replace("R", "L");
+Console.WriteLine($"Frase com dislalia: {fraseComDislalia}");
